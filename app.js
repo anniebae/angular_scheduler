411242388828b80e6563714515e2c8356fb741df
anniebae <bae.annie@gmail.com>
2015-02-25 17:05:36 -0500

    fixed firebase data load

---
+++ app.js
@@ -0,0 +1,40 @@
+angular.module('scheduleApp', ['firebase'])
+
+.controller('mainController', function($scope, $firebase) {
+
+  var ref = new Firebase("https://angular-scheduler.firebaseio.com/");  
+  var fb = $firebase(ref);
+
+  $scope.reset = function() {
+    fb.$set({
+      monday: {
+        name: 'Monday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          }
+        }
+      },
+      tuesday: {
+        name: 'Tuesday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          }
+        }
+      }
+    });
+
+  };
+
+});
